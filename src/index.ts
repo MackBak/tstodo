b0f44c4023b797c4fc538c1837a68eb3ead6afd7
Mack Bakkum <mackbakkum@gmail.com>
2024-10-09 21:37:55 +0200

    Finished simple tasklist assignment

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,71 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLFormElement>("#newTaskTitle")
+const tasks: Task[] = loadTasks()
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+tasks.forEach(addListItem)
+
+// Defining type Task with the attributes to be used.
+type Task = { 
+  id: string, 
+  title: string, 
+  completed: boolean, 
+  createdAt: Date
+}
+
+
+form?.addEventListener("submit", e => {
+
+  e.preventDefault() // prevents refresh on page
+
+  if (input?.value == "" || input?.value == null) return // the ? is optional chaining, if value doesnt exists it returns undefined
+
+  const newTask: Task = { 
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  tasks.push(newTask)
+  saveTasks()
+  
+
+  addListItem(newTask)
+  input.value = ""
+
+})
+
+// Function to add Task type to the todo list.
+function addListItem(task: Task) {
+  
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+
+}
+
+// Function to save task to local storage using JSON
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+
+// Function to load task from local storage. Returns an empty array if nothing found in LS.
+function loadTasks(): Task[] {
+const taskJson = localStorage.getItem("TASKS")
+if (taskJson == null) return []
+  return JSON.parse(taskJson)
+}
